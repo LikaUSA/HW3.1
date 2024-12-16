1676a799104de6c2a93ffef4825408049ef4c9b4
Andjela Burns <kaz_ang@yahoo.com>
2024-12-16 16:45:09 +0100

    changed code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
+
+        int price = 28789;
         int miles = service.calculate(price); // должно получиться 500
-        System.out.println(miles);
+        System.out.println("Miles for purchased ticket");
+        System.out.println(miles + " " + "Miles");
     }
 }
